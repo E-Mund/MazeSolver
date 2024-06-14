c9171ba3cda970b999ca83b4bd4cf791d7abdf67
E-Mund <ewm58972@uga.edu>
2024-06-14 10:12:24 -0400

    Added Point class and Line class to window.py

---
+++ main.sh
@@ -0,0 +1,3 @@
+#! /bin/zsh -e
+
+python src/main.py
--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
-from gui.window import Window
+from gui.window import *
 
 def main():
     win = Window(800, 600)
+    line = Line(Point(0, 0), Point(400, 300))
+    line.draw(win.canvas, "red")
     win.wait_for_close()
 
 if __name__=="__main__":
